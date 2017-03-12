c7b2e499ccf5c1b287a4a931a61b4a193533692a
sanwjt <252929154@qq.com>
2017-03-12 22:34:51 +0800

    init

---
+++ build/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var a = 1;
+var b = function b() {
+	alert(1);
+};
+var sum = function sum(a, b) {
+	return a + b;
+};
+
+console.log(0);
---
+++ src/index.js
@@ -0,0 +1,10 @@
+const a=1;
+const b=()=>{
+	alert(1)
+}
+const sum=(a,b)=> a+b;
+
+console.log(0)
+
+
+
